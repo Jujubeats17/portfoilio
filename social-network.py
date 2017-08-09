1988ef4a0d434c7f036d29b955c71e58eab129b7
Jujubeats17 <justine.peart@gmail.com>
2017-08-09 16:01:29 -0400

    Create social-network.py

---
+++ social-network.py
@@ -0,0 +1,20 @@
+class User:
+
+    def_init_(self, name):
+        self.user.name
+        self.age = 0
+        self.interests = []
+        self.friends = []
+
+
+class Network:
+    # Define the fields and methods for your object here.
+
+
+def main():
+    # Define the program flow for your user interface here.
+
+
+# Runs your program.
+if __name__ == '__main__':
+    main():
