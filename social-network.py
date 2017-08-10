75f24e0a3ecce25379f4e2407a4e2556ef704c96
Jujubeats17 <justine.peart@gmail.com>
2017-08-10 14:30:22 -0400

    Update social-network.py

--- social-network.py
+++ social-network.py
@@ -1,19 +1,43 @@
 class User:
 
-    def_init_(self, name):
-        self.user.name
+    def _init_(self, name):
+        self.user_name = name
         self.age = 0
         self.interests = []
         self.friends = []
 
+    def Change_username(self,name):
+        self.user_name = name
+
+    def age(self,age):
+        self.age=age
+
+    def interests(self,interests):
+        self.interests=interests
+
+    def friends(self,friends):
+        self.friends=friends
+
+
 
 class Network:
-    # Define the fields and methods for your object here.
+    def _init_(self,name):
+        self.name=name
+        self.users=[]
+
+    def name(self,name):
+        self.users.append(name)
+    def get_users(self):
+        return self.users
+
 
 
 def main():
-    # Define the program flow for your user interface here.
+    print ("What is your name?")
+    user_input=input()
 
+    print ("How old are you")
+    user_input=input()
 
 # Runs your program.
 if __name__ == '__main__':
