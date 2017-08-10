391adccbe60df083bcfcf2fdad89620c9e4a3ceb
Jujubeats17 <justine.peart@gmail.com>
2017-08-10 10:00:16 -0400

    Create aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,8 @@
+window.alert("ssssuuuuppp duuuuuude");
+
+//function myFunction() {
+  //  document.getElementById("demo").innerHTML = "OUCH! Why would you do that?!";
+
+function changebackground() {
+    document.getElementById("Lion").onclick = document.body.background.color = "blue";
+}
